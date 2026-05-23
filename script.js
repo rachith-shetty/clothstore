ca453b85974a58f0870617b903ddbd6a979aebd6
Rachith Shetty <rachithshetty77@gmail.com>
2026-05-23 12:13:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
-let menuItems = [
+let items = [
   { name:"Idli", price:40, img:"images/idli.jpg" },
-  { name:"Dosa", price:60, img:"images/dosa.jpg" }
+  { name:"Dosa", price:60, img:"images/dosa.jpg" },
+  { name:"Upma", price:50, img:"images/upma.jpg" },
+  { name:"Puri", price:55, img:"images/puri.jpg" }
 ];
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-const menuDiv = document.getElementById("menu");
-
-// show menu
-if(menuDiv){
-  menuItems.forEach((item, index)=>{
-    menuDiv.innerHTML += `
-      <div class="card">
-        <img src="${item.img}">
-        <h3>${item.name}</h3>
-        <p>₹${item.price}</p>
-
-        <button onclick="add(${index})">+</button>
-        <button onclick="remove(${index})">-</button>
-      </div>
-    `;
-  });
+// SAVE CART
+function save(){
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-// add item
+// GET QTY
+function getQty(name){
+  let item = cart.find(i => i.name === name);
+  return item ? item.qty : 0;
+}
+
+// SHOW MENU
+const menu = document.getElementById("menu");
+
+items.forEach((item, index)=>{
+
+  menu.innerHTML += `
+    <div class="card">
+      <img src="${item.img}">
+      <h3>${item.name}</h3>
+      <p>₹${item.price}</p>
+
+      <button onclick="add(${index})">+</button>
+
+      <span class="count" id="count-${index}">
+        ${getQty(item.name)}
+      </span>
+
+      <button onclick="remove(${index})">-</button>
+    </div>
+  `;
+});
+
+// ADD
 function add(i){
-  let item = menuItems[i];
+  let item = items[i];
 
   let found = cart.find(c => c.name === item.name);
 
   if(found){
     found.qty++;
-  } else {
+  }else{
     cart.push({...item, qty:1});
   }
 
-  localStorage.setItem("cart", JSON.stringify(cart));
+  save();
+  update(i);
 }
 
-// remove item
+// REMOVE
 function remove(i){
-  let item = menuItems[i];
+  let item = items[i];
 
   let found = cart.find(c => c.name === item.name);
 
@@ -52,5 +70,12 @@ function remove(i){
     }
   }
 
-  localStorage.setItem("cart", JSON.stringify(cart));
+  save();
+  update(i);
+}
+
+// UPDATE DISPLAY COUNT
+function update(i){
+  document.getElementById("count-"+i).innerText =
+    getQty(items[i].name);
 }
